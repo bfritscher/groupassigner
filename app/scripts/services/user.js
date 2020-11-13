c2d744535739df5a1bc88d2e4a871fed94721123
Boris Fritscher <boris@fritscher.ch>
2020-11-13 15:59:24 +0100

    fix google auth login new api

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -5,84 +5,62 @@ angular.module('groupAssignerApp')
 
     /* jshint camelcase: false */
     var self = this;
-    
+
     this.login = function(){
-      self.requireAuth(true).then(function () {
-        return self.authorize();
+      self.requireAuth(true).then(function (user) {
+        return self.authorize(user);
       });
     };
-    
+
     this.logout = function(){
-      gapi.auth.signOut();
+      gapi.auth2.getAuthInstance().signOut();
       $rootScope.user = '';
       $http.get(apiURL + '?action=logout')
       .success(function(data){
         self.courses = data;
       });
     };
-    
+
     this.getCourses = function(){
       $http.get(apiURL + '?action=listcourses')
       .success(function(data){
         self.courses = data;
       });
     };
-     
-    this.authorize = function(){
+
+    this.authorize = function(user){
       var deferred = $q.defer();
-      gapi.client.load('plus', 'v1', function() {
-        var request = gapi.client.plus.people.get({userId:'me'});
-        request.execute(function(result){
-          //got info from google, now validate on our server
-          $http.get(apiURL + '?action=login&user_id=' + result.id + '&token=' + gapi.auth.getToken().access_token)
+      var userId = user.getBasicProfile().getId();
+      $http.get(apiURL + '?action=login&user_id=' + userId + '&token=' + user.getAuthResponse(true).access_token)
           .success(function(data){
             deferred.resolve(data);
-            $rootScope.user = result;
+            $rootScope.user = user;
             self.courses = data;
           })
           .error(function(){
             //TODO could handle error responses display
-            console.log('Rejected userlogin with id ' + result.id);
+            console.log('Rejected userlogin with id ' + userId);
             deferred.reject();
           });
-        });
-      });
       $timeout(function(){
         deferred.reject('timeout');
       }, 10000);
       return deferred.promise;
     };
-    
-    this.requireAuth = function (immediateMode) {
-      var token = gapi.auth.getToken();
-      var now = Date.now() / 1000;
-      if (token && ((token.expires_at - now) > (60))) {
-        return $q.when(token);
-      } else {
-        var params = {
-          'client_id': config.clientId,
-          'scope': config.scopes,
-          'immediate': immediateMode
-        };
-        var deferred = $q.defer();
-        var doAuth = function doAuth(){
-          gapi.auth.authorize(params, function (result) {
-            if (result && !result.error) {
-              deferred.resolve(result);
-            } else {
-              //try not immediate
-              if(params.immediate){
-                params.immediate = false;
-                doAuth();
-              }else{
-                deferred.reject(result);
-              }
-            }
-            $rootScope.$digest();
-          });
-        };
-        doAuth();
-        return deferred.promise;
-      }
+
+    this.requireAuth = function () {
+      var deferred = $q.defer();
+      gapi.load('auth2', function() {
+        gapi.auth2.init(config).then(function(auth2) {
+          if(auth2.currentUser.get().isSignedIn()) {
+            deferred.resolve(auth2.currentUser.get());
+          } else {
+            auth2.signIn(config).then(function(user){
+              deferred.resolve(user);
+            });
+          }
+        });
+      });
+      return deferred.promise;
     };
   });
